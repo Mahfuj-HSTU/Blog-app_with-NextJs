518ddee7e3a028749b56149538abe07065cf3fc0
Mahfuj-HSTU <rmahfujur16@gmail.com>
2026-01-23 00:09:34 +0600

    create and use service to fetch user session

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { userService } from "@/services/user.service";
 
 export default async function Home() {
 
-  const session = await authClient.getSession()
-
-  console.log(session)
+  const session = await userService.getSession()
+  console.log({ session })
 
   return (
     <Button>
