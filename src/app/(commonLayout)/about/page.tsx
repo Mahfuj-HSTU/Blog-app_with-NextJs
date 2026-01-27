1e0e6512e527c5cbf1346ee8f9be1780f8748ef0
Mahfuj-HSTU <rmahfujur16@gmail.com>
2026-01-27 23:56:10 +0600

    add blog card to the about page

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import { getBlogs } from "@/actions/blog.action";
+import BlogCard from "@/components/modules/homepage/BlogCard";
+import { TBlog } from "@/types";
 import { useEffect, useState } from "react";
 
 const AboutPage = () => {
-  const [data, setData] = useState()
+  const [data, setData] = useState<{ data: TBlog[] } | null>(null)
 
   useEffect(() => {
     (async () => {
@@ -12,13 +14,15 @@ const AboutPage = () => {
     })()
   }, [])
 
-  console.log({ data })
+  console.log(data)
 
   return (
-    <div>
-      <h1>
-        This is about page
-      </h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      {
+        data?.data?.map((post: TBlog) => (
+          <BlogCard key={post.id} post={post} />
+        ))
+      }
     </div>
   );
 };
